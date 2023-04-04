d959e14c9aee2c7cbdc74b124e8efacb773b3316
Kristian Lieberg <liebergka@gmail.com>
2023-04-04 16:22:51 -0700

    Update configuration, add reading from dotenv

--- src/app.py
+++ src/app.py
@@ -1,13 +1,21 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
 
 app = Flask(__name__)
 
 PLAYLIST_ID = ''
+load_dotenv()
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    spotify_client_id = os.environ.get("spotifyClientId")
+    spotify_secret = os.environ.get("spotifySecret")
+    return "<p>userID= " + spotify_client_id + "; key = " + spotify_secret + "</p>"
 
 @app.route("/songs", methods=['PUT', 'GET'])
 def add_song():
     return "<p>UNDER CONSTRUCTION</p>"
+    
